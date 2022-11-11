4f0dbcc2aa2a6a52d474b90341ab1147e328b9fe
Louis Wu <lwu@netskope.com>
2022-11-11 08:27:40 +0800

    Check IP_OPTIONS from getsockopt

--- socket/clientsocket.c
+++ socket/clientsocket.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #define buffersize 1024
 
@@ -25,14 +26,14 @@ int main(int argc, char *argv[]){
         perror("Error setting options");
         close(sock);
     }
-    // This set doesn't support SOCK_STREAM, refer to
-    // https://man7.org/linux/man-pages/man7/ip.7.html section IP_RECVOPTS
-    int option = 1;
-    if (setsockopt(sock, IPPROTO_IP, IP_RECVOPTS, (char *)&option, sizeof(option)) == -1) {
-        perror("Error setting IP_RECVOPTS");
-        close(sock);
-     exit(1);
-    }
+    // // This set doesn't support SOCK_STREAM, refer to
+    // // https://man7.org/linux/man-pages/man7/ip.7.html section IP_RECVOPTS
+    // int option = 1;
+    // if (setsockopt(sock, IPPROTO_IP, IP_RECVOPTS, (char *)&option, sizeof(option)) == -1) {
+    //     perror("Error setting IP_RECVOPTS");
+    //     close(sock);
+    //  exit(1);
+    // }
     
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -44,50 +45,69 @@ int main(int argc, char *argv[]){
     const char *message = "11";
     printf("send\n");
     send(sock, message, strlen(message)+1, 0);
+    
     printf("send done\n");
-   { // Since IP_RECVOPTS isn't supported, `cmhdr` is always NULL
-      // just put it here to prove that it's not supported
-      struct sockaddr_in sin_recv;
-      char mes[1500];
-      struct msghdr mhdr;
-      struct iovec iov;
-      struct cmsghdr *cmhdr;
-      char control[1000];
-      int len;
-      unsigned char *recv_options;
-      unsigned int i;
+//    { // Since IP_RECVOPTS isn't supported, `cmhdr` is always NULL
+//       // just put it here to prove that it's not supported
+//       struct sockaddr_in sin_recv;
+//       char mes[1500];
+//       struct msghdr mhdr;
+//       struct iovec iov;
+//       struct cmsghdr *cmhdr;
+//       char control[1000];
+//       int len;
+//       unsigned char *recv_options;
+//       unsigned int i;
 
-      mhdr.msg_name = &sin_recv;
-      mhdr.msg_namelen = sizeof(sin_recv);
-      mhdr.msg_iov = &iov;
-      mhdr.msg_iovlen = 1;
-      mhdr.msg_control = &control;
-      mhdr.msg_controllen = sizeof(control);
-      iov.iov_base = mes;
-      iov.iov_len = sizeof(mes);
-      if ((len = recvmsg(sock, &mhdr, 0)) == -1) {
-        perror("Error receiving");
-    } else {
-        cmhdr = CMSG_FIRSTHDR(&mhdr); // In SOCK_STREAM, `cmhdr` is always NULL
-        printf("cmhdr address %p\n", cmhdr);
-        while (cmhdr) {
-            if (cmhdr->cmsg_level == IPPROTO_IP &&
-                    cmhdr->cmsg_type == IP_RECVOPTS) {
-                recv_options = CMSG_DATA(cmhdr);
-                printf("options: ");
-                for (i=0;i<cmhdr->cmsg_len-sizeof(struct cmsghdr);i++) {
-                    printf("%02x ", recv_options[i]);
-                }
-                printf("\n");
-            }
-            cmhdr = CMSG_NXTHDR(&mhdr, cmhdr);
-        }
-    }
-    }
+//       mhdr.msg_name = &sin_recv;
+//       mhdr.msg_namelen = sizeof(sin_recv);
+//       mhdr.msg_iov = &iov;
+//       mhdr.msg_iovlen = 1;
+//       mhdr.msg_control = &control;
+//       mhdr.msg_controllen = sizeof(control);
+//       iov.iov_base = mes;
+//       iov.iov_len = sizeof(mes);
+//       if ((len = recvmsg(sock, &mhdr, 0)) == -1) {
+//         perror("Error receiving");
+//     } else {
+//         cmhdr = CMSG_FIRSTHDR(&mhdr); // In SOCK_STREAM, `cmhdr` is always NULL
+//         printf("cmhdr address %p\n", cmhdr);
+//         while (cmhdr) {
+//             if (cmhdr->cmsg_level == IPPROTO_IP &&
+//                     cmhdr->cmsg_type == IP_RECVOPTS) {
+//                 recv_options = CMSG_DATA(cmhdr);
+//                 printf("options: ");
+//                 for (i=0;i<cmhdr->cmsg_len-sizeof(struct cmsghdr);i++) {
+//                     printf("%02x ", recv_options[i]);
+//                 }
+//                 printf("\n");
+//             }
+//             cmhdr = CMSG_NXTHDR(&mhdr, cmhdr);
+//         }
+//     }
+//     }
 
     char *buffer = (char*) calloc(buffersize, sizeof(char)) ;
     printf("read\n");
     read(sock, buffer, buffersize);
+
+    // Get ip options set on sock
+    // Result: 
+    struct ip_opts getopt = {0};
+    socklen_t optlen = sizeof(getopt);
+    if (getsockopt(sock, IPPROTO_IP, IP_OPTIONS, (char *)&getopt, &optlen)== -1) {
+        perror("Error get setting options");
+        close(sock);
+    }
+    printf("retrieved options dst %u\n", getopt.ip_dst.s_addr);
+    for(int i = 0; i < 4; i++){
+        printf("retrieved options dst %u\n", (getopt.ip_dst.s_addr >> (i*2)) & 0xFF );
+    }
+    printf("====ip_opts start ====\n");
+    for(size_t i = 0; i < sizeof(getopt.ip_opts); i++){
+        printf("%d ", getopt.ip_opts[i]);
+    }
+    printf("\n====ip_ipts end====\n");
 
     printf("result form server: %s\n", buffer);
 
